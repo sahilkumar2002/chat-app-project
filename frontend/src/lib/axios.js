612aa89ec430679c8e08c301b174ef529730ba72
sahilkumar2002 <154125748+sahilkumar2002@users.noreply.github.com>
2025-09-20 01:29:05 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-export const axiosInstance =axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-project-tggx.onrender.com" : "/api",
-    withCredentials:true,
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api" // 👈 dev mode backend
+    : "https://chat-app-project-backend-gixv.onrender.com/api", // 👈 prod backend
+  withCredentials: true, // 👈 ensure cookies are sent
 });
